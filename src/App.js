9fcdfe362448ddfcfaa5c7259080328c6112f2c6
4edorov <4edorov@gmail.com>
2017-12-05 07:37:13 +0300

    cycles and squares

--- src/App.js
+++ src/App.js
@@ -4,7 +4,25 @@ import './App.css'
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className='app'>
+        <div className='main-cycle'>
+          <div className='frame-cycle'>
+            <div className='square'>
+              <div className='top-part'>
+                <div className='top-left'>
+                </div>
+                <div className='top-right'>
+                </div>
+              </div>
+              <div className='bottom-part'>
+                <div className='bottom-left'>
+                </div>
+                <div className='bottom-right'>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
